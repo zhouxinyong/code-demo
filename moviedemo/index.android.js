ae6fd65cc06ab653e73e06b445b7254ee6c1245a
zhouxinyong <zhoudaxia5683@163.com>
2016-04-12 21:45:40 +0800

    add moviedemo

---
+++ moviedemo/index.android.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ListView
+}
+from 'react-native';
+
+import Movie from './component/movie';
+
+class MovieProject extends Component {
+
+  render() {
+    return (
+      <Movie/>
+    )
+  }
+}
+
+
+
+AppRegistry.registerComponent('MovieProject', () => MovieProject);
